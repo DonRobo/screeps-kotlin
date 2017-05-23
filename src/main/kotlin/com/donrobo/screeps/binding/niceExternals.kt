547aa04f6c14e65805e8bdaf6db4fcbc2d05131b
Robert Tieber <robert.tieber@gmail.com>
2017-05-23 02:32:14 +0200

    Use extension functions instead of new classes

--- src/main/kotlin/com/donrobo/screeps/binding/niceExternals.kt
+++ src/main/kotlin/com/donrobo/screeps/binding/niceExternals.kt
@@ -1,12 +1,15 @@
 package com.donrobo.screeps.binding
 
-import com.donrobo.screeps.binding.externals.*
+import com.donrobo.screeps.binding.externals.ConstructionSite
+import com.donrobo.screeps.binding.externals.Game
+import com.donrobo.screeps.binding.externals.Structure
+import com.donrobo.screeps.binding.externals.StructureSpawn
 
 class GameKt {
     companion object {
-        val spawns: Map<String, StructureSpawnKt>
+        val spawns: Map<String, StructureSpawn>
             get() {
-                return (toMap(Game.spawns) as Map<String, StructureSpawn>).mapValues { StructureSpawnKt(it.value) }
+                return (toMap(Game.spawns) as Map<String, StructureSpawn>)
             }
 
         val structures: Map<String, Structure>
@@ -20,42 +23,31 @@ class GameKt {
     }
 }
 
-class StructureSpawnKt(val structureSpawn: StructureSpawn) {
-    val energy: Int get() = structureSpawn.energy
-    val energyCapacity: Int get() = structureSpawn.energyCapacity
-    val memory: Any get() = structureSpawn.memory
-    val name: String get() = structureSpawn.name
-    val spawning: SpawningValue get() = structureSpawn.spawning
-    fun canCreateCreep(body: List<CreepPart>, name: String? = null): Status {
-        val status = structureSpawn.canCreateCreep(body.asSequence().map { it.value }.toJsArray(), name)
+fun StructureSpawn.canCreateCreepKt(body: List<CreepPart>, name: String? = null): Status {
+    val status = this.canCreateCreep(body.asSequence().map { it.value }.toJsArray(), name)
 
-        return Status.fromValue(status)
-    }
-
-    fun createCreep(body: List<CreepPart>, name: String? = null, memory: Any? = null): CreepCreationStatus {
-        val result = structureSpawn.createCreep(body.asSequence().map { it.value }.toJsArray(), name, memory)
-
-        val status: Status
-        val resultName: String?
-        if (result is String) {
-            status = Status.OK
-            resultName = result
-        } else if (result is Int) {
-            status = Status.fromValue(result)
-            resultName = null
-        } else {
-            throw RuntimeException("Unexpected result type")
-        }
-
-        return CreepCreationStatus(status, resultName)
-    }
-//    fun renewCreep(target: Creep): Number
-//    fun recycleCreep(target: Creep): Number
-//    fun transferEnergy(target: Creep, amount: Number? = definedExternally): Number
-
-    data class CreepCreationStatus(val status: Status, val name: String?)
+    return Status.fromValue(status)
 }
 
+fun StructureSpawn.createCreepKt(body: List<CreepPart>, name: String? = null, memory: Any? = null): CreepCreationStatus {
+    val result = this.createCreep(body.asSequence().map { it.value }.toJsArray(), name, memory)
+
+    val status: Status
+    val resultName: String?
+    if (result is String) {
+        status = Status.OK
+        resultName = result
+    } else if (result is Int) {
+        status = Status.fromValue(result)
+        resultName = null
+    } else {
+        throw RuntimeException("Unexpected result type")
+    }
+
+    return CreepCreationStatus(status, resultName)
+}
+
+data class CreepCreationStatus(val status: Status, val name: String?)
 
 enum class Status(val value: Int) {
     OK(com.donrobo.screeps.binding.externals.OK),
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,7 @@
 import com.donrobo.screeps.binding.CreepPart
 import com.donrobo.screeps.binding.GameKt
 import com.donrobo.screeps.binding.Status
+import com.donrobo.screeps.binding.canCreateCreepKt
 import com.donrobo.screeps.binding.externals.Game
 import com.donrobo.screeps.binding.externals.StructureSpawn
 
@@ -10,7 +11,7 @@ fun loop() {
     GameKt.spawns.values.asSequence()
             .filter { it.energy >= it.energyCapacity }
             .forEach { spawn ->
-                if (spawn.canCreateCreep(listOf(CreepPart.MOVE, CreepPart.CARRY)) == Status.OK)
+                if (spawn.canCreateCreepKt(listOf(CreepPart.MOVE, CreepPart.CARRY)) == Status.OK)
                     println("${spawn.name} could spawn something")
             }
     println("CPU: ${Game.cpu.getUsed()}")
